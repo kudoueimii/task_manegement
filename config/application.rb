de36686d2dce371be6b043dbfd7a606bcd664bef
bargholz eimii <818_77_@ezweb.ne.jp>
2022-10-27 17:43:49 +0900

    エラー日本語化

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module TaskManegement
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.time_zone= 'Tokyo'
+    config.active_record.default_timezone= :local
 
     config.generators do |g|
       g.test_framework :rspec,
